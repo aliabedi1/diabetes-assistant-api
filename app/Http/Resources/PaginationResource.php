ae357d1288b57fb9452c8d9a42c29f27a6996189
ali abedi <ali.abedi9731@yahoo.com>
2026-05-20 08:57:20 +0330

    feat: add pagination resource

---
+++ app/Http/Resources/PaginationResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaginationResource extends JsonResource
+{
+    protected $rawResource;
+
+
+    public function __construct($resource)
+    {
+        $this->rawResource = $resource;
+        parent::__construct($resource);
+    }
+
+
+    public function toArray($request)
+    {
+        return [
+            'items'      => $this->collection,
+            'pagination' => [
+                'current_page' => $this->rawResource->currentPage(),
+                'from'         => $this->rawResource->firstItem(),
+                'last_page'    => $this->rawResource->lastPage(),
+                'per_page'     => $this->rawResource->perPage(),
+                'to'           => $this->rawResource->lastItem(),
+                'total'        => $this->rawResource->total(),
+            ],
+            'links'      => [
+                'first' => $this->rawResource->url(1),
+                'last'  => $this->rawResource->url($this->rawResource->lastPage()),
+                'prev'  => $this->rawResource->previousPageUrl(),
+                'next'  => $this->rawResource->nextPageUrl(),
+            ]
+        ];
+    }
+}
