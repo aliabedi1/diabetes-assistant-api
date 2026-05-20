da7f8ccb1190dc051348c17ac9d1b1d7526757e9
ali abedi <ali.abedi9731@yahoo.com>
2026-05-20 16:32:31 +0330

    feat: move old controllers

---
+++ app/Http/Controllers/Api/V1/Glucose/GlucoseLogController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Glucose;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Glucose\GlucoseLogIndexRequest;
+use App\Http\Requests\Api\V1\Glucose\GlucoseLogStoreRequest;
+use App\Http\Resources\Api\V1\Glucose\GlucoseLogResource;
+use App\Http\Resources\PaginationResource;
+use App\Models\GlucoseLog;
+use Illuminate\Support\Facades\Response;
+
+class GlucoseLogController extends Controller
+{
+    public function index(GlucoseLogIndexRequest $request)
+    {
+        return Response::success(
+            data: new PaginationResource(
+                GlucoseLogResource::collection(
+                    $request->user()
+                        ->glucoseLogs()
+                        ->latest()
+                        ->paginate()
+                )
+            )
+        );
+    }
+
+
+    public function store(GlucoseLogStoreRequest $request)
+    {
+        return Response::store(
+            new GlucoseLogResource(
+                GlucoseLog::query()->create([
+                    'user_id' => $request->user()->id,
+                    ...$request->validated(),
+                ])
+            )
+        );
+    }
+}
--- app/Http/Controllers/Api/V1/Injection/InjectionLogController.php
+++ app/Http/Controllers/Api/V1/Injection/InjectionLogController.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+namespace App\Http\Controllers\Api\V1\Injection;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class InjectionLogController extends Controller
 {
